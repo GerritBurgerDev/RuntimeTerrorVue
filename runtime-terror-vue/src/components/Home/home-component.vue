f04184496363c5146932711aa525ab9bf9336b82
GerritBurgerActivate <gerrit.burger@impact.com>
2022-03-30 11:34:22 +0200

    Initial Build Setup

---
+++ runtime-terror-vue/src/components/Home/home-component.vue
@@ -0,0 +1,10 @@
+<template src="./home-component.html"></template>
+<style src="./home-component.scss"></style>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const props = defineProps<{ msg: string }>()
+
+const count = ref(0);
+</script>
