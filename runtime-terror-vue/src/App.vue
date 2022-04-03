b248fbfd262ed57783bbdf1e92f6ad4798f7834c
GerritBurgerActivate <gerrit.burger@impact.com>
2022-04-03 18:11:32 +0200

    Add css v-bind example to app component and install vitest

--- runtime-terror-vue/src/App.vue
+++ runtime-terror-vue/src/App.vue
@@ -4,24 +4,43 @@
       <nav-component></nav-component>
       <router-view></router-view>
     </v-main>
+    <footer-component></footer-component>
   </v-app>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import NavComponent from "./components/Nav/nav-component.vue";
+import FooterComponent from "./components/Footer/footer-component.vue";
 
 export default defineComponent({
   name: 'App',
 
   components: {
+    FooterComponent,
     NavComponent,
   },
 
   data () {
     return {
-      // None so far.
+      footerHeight: '0px',
     }
   },
+  mounted() {
+    this.footerHeight = this.getFooterHeight();
+  },
+  methods: {
+    getFooterHeight: () => {
+      const element = document.querySelector('.v-footer');
+      const height = element ? element.clientHeight : 0;
+      return (height - 50) + 'px';
+    }
+  }
 })
 </script>
+
+<style scoped>
+.v-main {
+  padding-bottom: v-bind(footerHeight) !important;
+}
+</style>
